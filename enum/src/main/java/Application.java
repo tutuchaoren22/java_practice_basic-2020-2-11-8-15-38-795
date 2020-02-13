89ce72e68d90853c096de7d44e841889b6dec2d8
duyuqian <15738381799@163.com>
2020-02-13 23:45:09 +0800

    [duyuqian] refactor: replace '==' with equals method

--- enum/src/main/java/Application.java
+++ enum/src/main/java/Application.java
@@ -2,7 +2,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("所有女学生的信息为：");
         for (Student student : Student.values()) {
-            if (student.getGender() == "Male") {
+            if (student.getGender().equals("Male")) {
                 System.out.println("(" + student.getName() + "," + student.getGender() + ")");
             }
         }
